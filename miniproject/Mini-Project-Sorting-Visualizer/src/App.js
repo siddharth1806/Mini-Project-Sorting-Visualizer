a4138327ce0991c855224f016b51125b4833ecd7
Siddharth Singh <siddharth.singh1_cs18@gla.ac.in>
2020-11-24 17:57:59 +0530

    sorting visualizer

---
+++ miniproject/Mini-Project-Sorting-Visualizer/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import SortingVisualizer from './SortingVisualizer/SortingVisualizer.jsx'
+
+function App() {
+  return (
+    <div>
+      <SortingVisualizer />
+    </ div>
+  );
+}
+
+export default App;
